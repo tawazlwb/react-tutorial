58aea82c8310cef6967d02112212488fa059f2f7
Khairy Ismail <ikheiry@sqli.com>
2022-09-30 15:23:16 +0200

    31. Event Basics

--- src/Book.js
+++ src/Book.js
@@ -1,11 +1,25 @@
 import React from 'react'
 
 export const Book = ({ img, title, auther }) => {
+  const clickHandler = (e) => {
+    console.log(e)
+    console.log(e.target)
+    alert('Hello world!')
+  }
+
+  const complexExampleHandler = (auther) => console.log(auther)
+
   return (
-    <article className='book'>
+    <article className='book' onMouseOver={() => console.log(title)}>
       <img src={img} alt='' />
-      <h1>{title}</h1>
+      <h1 onClick={() => console.log(title)}>{title}</h1>
       <h4>{auther}</h4>
+      <button type='button' onClick={clickHandler}>
+        refenrence example
+      </button>
+      <button type='button' onClick={() => complexExampleHandler(auther)}>
+        more complex example
+      </button>
     </article>
   )
 }
